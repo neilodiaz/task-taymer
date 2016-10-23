210cd82730a9bcc8830d5bd4a191da3abc95c3d3
Neiljun I. Odiaz <neiljun.odiaz@gmail.com>
2016-10-23 10:43:10 +0800

    Resume timer when browser reloads, if available

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -10,11 +10,15 @@ function todoListController($scope, $http)
 	var timer_status = "run";
 
     $scope.todos = [];
+	$scope.newtodo = '';
+	$scope.hours = 0;
+	$scope.mins = 1;
     $scope.countdown = '';
     $scope.active_todo_name = '';
     $scope.timer_status = "Pause";
     $scope.timer_icon = "pause";
     $scope.timer_started = false;
+    $scope.timer_paused = false;
 
     $scope.getToDoList = function()
     {
@@ -22,6 +26,7 @@ function todoListController($scope, $http)
     	if ( local_todo ) {
     		$scope.todos = local_todo;
     	}
+    	setTimeout(checkActiveTodo, 10);
     	notifyMe();
     }
 
@@ -32,6 +37,7 @@ function todoListController($scope, $http)
     		'name': $scope.newtodo,
     		'hours': $scope.hours,
     		'mins': $scope.mins,
+    		'secs': 0
     	};
     	$scope.todos.push( todo );
 
@@ -46,6 +52,10 @@ function todoListController($scope, $http)
 
     	// clear textbox
     	$scope.newtodo = '';
+    	$scope.hours = 0;
+    	$scope.mins = 1;
+
+    	return false;
     }
 
     $scope.removeTodo = function(index)
@@ -63,7 +73,11 @@ function todoListController($scope, $http)
     	var timer_btn = angular.element( event.target );
 
     	// Set active/running todo
-    	localStorage.setItem('active_todo', index);
+    	var active = {
+			'index': index,
+			'todo': select_todo
+		};
+    	localStorage.setItem('active_todo', JSON.stringify(active));
     	initiateEasyTimer(select_todo, timer_btn);
     }
 
@@ -94,18 +108,26 @@ function todoListController($scope, $http)
     function initiateEasyTimer(select_todo, timer_btn) {
     	var mins = parseInt(select_todo.mins);
     	var hours = parseInt(select_todo.hours);
+    	var secs = parseInt(select_todo.secs);
 
-    	var start_values = { minutes:mins,hours:hours };
+    	var start_values = { seconds:secs,minutes:mins,hours:hours };
 
-    	timer = new Timer();
+    	// @TODO: Effectively create a function for checking pause and resume function;
+    	if ( timer_status == "run" && $scope.timer_paused == false) {
+    		timer = new Timer();
+    	}
 
-    	if ( !timer.isRunning() ) {
+    	console.log(timer_status);
+    	console.log($scope.timer_paused);
+
+    	if ( timer_status == "run" && $scope.timer_paused == false) {
     		timer.start({countdown: true, startValues: start_values});
     		timer_btn.addClass('active-task');
     		timer_btn.removeClass('fa-play').addClass('fa-pause');
     		timer_status = "run";
     		$scope.timer_icon = "pause";
     		$scope.timer_status = "Pause";
+    		$scope.timer_paused = false;
     		console.log('Timer started');
     	} else {
     		timer.pause();
@@ -113,6 +135,7 @@ function todoListController($scope, $http)
     		timer_status = "paused";
     		$scope.timer_icon = "play";
     		$scope.timer_status = "Resume";
+    		$scope.timer_paused = true;
     		console.log('Timer paused');
     	}
 
@@ -129,6 +152,14 @@ function todoListController($scope, $http)
 
 		timer.addEventListener('secondsUpdated', function (e) {
 		    document.getElementById('countdown_timer').innerHTML = timer.getTimeValues().toString();
+
+		    var active_todo = JSON.parse(localStorage.getItem('active_todo'));
+	    	if ( active_todo ) {
+	    		active_todo.todo.hours = timer.getTimeValues().hours;
+	    		active_todo.todo.mins = timer.getTimeValues().minutes;
+	    		active_todo.todo.secs = timer.getTimeValues().seconds;
+		    	localStorage.setItem('active_todo', JSON.stringify(active_todo));
+	    	}
 		});
     }
 
@@ -143,7 +174,7 @@ function todoListController($scope, $http)
 		    }
 		    var notification = new Notification( 'Taymer', options );
 		    var audio = new Audio('audio/alert.mp3');
-			audio.play();
+			// audio.play();
 		});
     }
 
@@ -153,8 +184,8 @@ function todoListController($scope, $http)
     }
 
     function nextTodo() {
-    	var current_index = localStorage.getItem('active_todo');
-    	var new_index = parseInt(current_index) + 1;
+    	var active_todo = JSON.parse(localStorage.getItem('active_todo'));
+    	var new_index = parseInt(active_todo.index) + 1;
     	var todo = JSON.parse(localStorage.getItem('todos'));
     	var select_todo = todo[new_index];
     	var active = document.getElementsByClassName('active-task');
@@ -162,7 +193,11 @@ function todoListController($scope, $http)
 
     	// Check if there's a next task on the list
     	if ( todo.length > new_index ) {
-	    	localStorage.setItem('active_todo', new_index);
+    		var active = {
+    			'index': new_index,
+    			'todo': select_todo
+    		};
+	    	localStorage.setItem('active_todo', JSON.stringify(active));
 
 	    	$scope.stopTimer();
 	    	initiateEasyTimer(select_todo, next_btn);
@@ -173,6 +208,10 @@ function todoListController($scope, $http)
 		    var notification = new Notification( 'Taymer', options );
 	    } else {
 	    	$scope.stopTimer();
+	    	$scope.timer_started = false;
+	    	localStorage.removeItem('active_todo');
+	    	var running_todo_bar = document.getElementById('running_todo');
+    		angular.element(running_todo_bar).addClass('ng-hide');
 	    }
     }
 
@@ -194,5 +233,19 @@ function todoListController($scope, $http)
 			}
 			});
 		}
+    }
+
+    function checkActiveTodo()
+    {
+    	var active_todo = JSON.parse(localStorage.getItem('active_todo'));
+    	if ( active_todo ) {
+    		var active_element = "todokey_" + active_todo.index;
+    		var active_btn = document.getElementById(active_element);
+    		$scope.timer_started = true;
+    		$scope.active_todo_name = active_todo.name;
+    		var running_todo_bar = document.getElementById('running_todo');
+    		angular.element(running_todo_bar).removeClass('ng-hide');
+    		initiateEasyTimer(active_todo.todo, angular.element(active_btn).find('.action-btn'));
+    	}
     }
 }
